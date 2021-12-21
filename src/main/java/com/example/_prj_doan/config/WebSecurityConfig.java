6b06952b75118272d5fbdb6c47e7180347ee2425
TheNN <thenguyenngochn@gmail.com>
2021-12-21 10:14:49 +0700

    them form login

--- src/main/java/com/example/_prj_doan/config/WebSecurityConfig.java
+++ src/main/java/com/example/_prj_doan/config/WebSecurityConfig.java
@@ -18,6 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll();
     }
 }
