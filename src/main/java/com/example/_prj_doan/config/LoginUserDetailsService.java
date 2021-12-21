229e916f86382022c4cde4d51377ad6ed6ed34a3
TheNN <thenguyenngochn@gmail.com>
2021-12-21 20:25:40 +0700

    authority

---
+++ src/main/java/com/example/_prj_doan/config/LoginUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example._prj_doan.config;
+
+import com.example._prj_doan.entity.User;
+import com.example._prj_doan.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class LoginUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.getUserByEmail(email);
+        if(user != null){
+            return new LoginUserDetails(user);
+        }
+        throw new UsernameNotFoundException("Không tìm thấy người dùng có Email: "+ email);
+    }
+}
--- src/main/java/com/example/_prj_doan/config/WebSecurityConfig.java
+++ src/main/java/com/example/_prj_doan/config/WebSecurityConfig.java
@@ -2,18 +2,30 @@ package com.example._prj_doan.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new LoginUserDetailsService();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
     }
 
     @Override
@@ -21,4 +33,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll();
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+       web.ignoring().antMatchers("/images/**","/js/**", "/webjars/**");
+    }
+
+
 }
